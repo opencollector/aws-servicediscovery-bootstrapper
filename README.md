e038ec0e4fe9ed945c54238dccf0126a3b373b9c
Moriyoshi Koizumi <moriyoshi.koizumi@opencollector.jp>
2025-05-11 13:07:03 +0900

    Initial.

---
+++ README.md
@@ -0,0 +1,126 @@
+# AWS ServiceDiscovery (a.k.a. Cloud Map) bootstrapper
+
+AWS ServiceDiscovery bootstrapper is a helper utility that enables any executables to run with arguments interpolated with instance attributes associated in CloudMap services.
+
+For example, if you have a CloudMap namespace `my-namespace` and a service `my-service` with two instances `192.168.0.2` and `192.168.0.3`, each assigned a port attribute of `8000`, running the following command will invoke `my-executable` with the argument `--servers=http://192.168.0.2:8000,http://192.168.0.3:8000`
+
+```bash
+aws-service-discovery-bootstrapper \
+  -namespace my-namespace \
+  <executable> \
+    '--servers={{ instances "my-service" | extract "IPv4Addr,Port" | mapprintf "http://%s:%d" | join "," }}'
+```
+
+## Installation
+
+You can install the AWS ServiceDiscovery bootstrapper using `go get`:
+
+```bash
+go install @github.com/opencollector/aws-service-discovery-bootstrapper@latest
+```
+
+## Usage
+
+```bash
+aws-service-discovery-bootstrapper \
+  -namespace <namespace> \
+  -health-status <health-status> \
+  -retry <retry-count> \
+  -execution-delay-jitter <delay> \
+  -execution-delay-jitter-unit <unit> \
+  [-no-fail] \
+  -- <executable> [args...]
+```
+
+<dl>
+<dt><code>-namespace</code></dt>
+<dd>
+
+**Required.** The namespace to use for service discovery.
+</dd>
+<dt><code>-health-status</code></dt>
+<dd>
+
+The health status to use for service discovery. Default is `HEALTHY`.
+
+Valid values are:
+- `HEALTHY`: Include only healthy instances.
+- `UNHEALTHY`: Include only unhealthy instances.
+- `ALL`: Include all instances.
+- `HEALTHY_OR_ELSE_ALL`: Include only healthy instances, or all instances if no healthy instances are found.
+</dd>
+<dt><code>-retry</code></dt>
+<dd>
+The number of times to retry service discovery if no instances that matches the specified health status are found. Default is `3`.
+</dd>
+<dt><code>-execution-delay-jitter</code></dt>
+<dd>
+
+The amount of jitter that delays the command execution. This is useful to give more chance to the command to run successfully if the services being discovered are not available yet.  The amount can be specified with a suffix of `s` (seconds), `ms` (milliseconds), `us` (microseconds), or `ns` (nanoseconds). Default is `0s`.
+</dd>
+<dt><code>-execution-delay-jitter-unit</code></dt>
+<dd>
+
+The unit of the execution delay jitter. Some of valid values are `1s` (seconds), `1ms` (milliseconds), `1us` (microseconds), `1ns` (nanoseconds). Default is `1s`.
+</dd>
+<dt><code>-no-fail</code></dt>
+<dd>
+
+If specified, `instances` function will not fail if no instances that match the specified health status are found. Note that retries will still be attempted if this option is specified.
+</dd>
+<dt><code>&lt;executable&gt;</code></dt>
+<dd>
+
+**Required.** The executable to run with the interpolated arguments.
+</dd>
+<dt><code>[args...]</code></dt>
+<dd>
+
+The arguments to pass to the executable. These can include interpolated values from CloudMap services. How the interpolation works is described below.
+</dl>
+
+## Interpolation
+
+The AWS ServiceDiscovery bootstrapper uses the [go-template](https://golang.org/pkg/text/template/) syntax for interpolation. The following functions are available:
+
+- `instances <service-name>`: Returns a slice of structs that describes instances for the specified service name.
+    Each struct contains the following fields:
+    - `IPv4Addr`: The IPv4 address of the instance.
+    - `IPv6Addr`: The IPv6 address of the instance.
+    - `Port`: The port of the instance.
+
+- `extract <attribute> <slice>`: For each item of a slice, extracts the specified attribute(s) from the instances, and returns the slice of slices of extracted attributes. `<attribute>` can be a comma-separated list of attributes (e.g. `IPv4Addr,Port`).
+
+    Example: `extract "IPv4Addr,Port"` will return a slice of slices, where each inner slice contains the IPv4 address and port of an instance.
+
+- `exclude <ip-addr> <slice>`: Excludes a item whose any of IP addresses corresponds to the specified IP address from the slice. The IP address can be an IPv4 or IPv6 address.
+
+    Example: `exclude (ifaddr "192.168.0.0/24")` will exclude the instance whose IPv4 address matches the host's IP address.
+
+- `mapprintf <format> <input>`: For each item of a slice, formats the value with the specified format string. The format is done using the [fmt.Sprintf](https://golang.org/pkg/fmt/#Sprintf) syntax.
+   
+   Example: `mapprintf "http://%s:%d"` will format the IPv4 address and port of each instance into a URL.
+
+- `join <separator> <input>`: Joins the items of a slice into a single string, separated by the specified separator.
+
+    Example: `join ","` will join the items of a slice with a comma.
+
+- `ifaddr <CIDR>`: Returns the address that matches the CIDR if exists.
+
+    Example: `ifaddr 192.168.0.0/24` will return the IPv4 address of the instance that matches the CIDR.
+
+## Configuration
+
+The AWS ServiceDiscovery bootstrapper can be configured using environment variables supported by AWS SDK. The following is a non-exhaustive list of such:
+
+- `AWS_REGION`: The AWS region to use for service discovery. If not set, the default region from the AWS CLI configuration will be used.
+
+- `AWS_PROFILE`: The AWS profile to use for service discovery. If not set, the default profile from the AWS CLI configuration will be used.
+
+- `AWS_ACCESS_KEY_ID`: The AWS access key ID to use for service discovery. If not set, the default credentials from the AWS CLI configuration will be used.
+
+- `AWS_SECRET_ACCESS_KEY`: The AWS secret access key to use for service discovery. If not set, the default credentials from the AWS CLI configuration will be used.
+
+- `AWS_SESSION_TOKEN`: The AWS session token to use for service discovery. If not set, the default credentials from the AWS CLI configuration will be used.
+
+- `AWS_ENDPOINT_URL`: The AWS endpoint URL to use for service discovery. If not set, the default endpoint URL from the AWS CLI configuration will be used. Specifying this will effectively disable the endpoint prefixing behavior. (Thus the actual endpoint will end up being the same as the endpoint URL, in contrast to `data-servicediscovery.<region>.amazonaws.com` where the endpoint is `servicediscovery.<region>.amazonaws.com`.)
---
+++ main.go
@@ -0,0 +1,423 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log/slog"
+	"math/rand/v2"
+	"net"
+	"net/netip"
+	"os"
+	"os/exec"
+	"reflect"
+	"slices"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/servicediscovery"
+	servicediscovery_types "github.com/aws/aws-sdk-go-v2/service/servicediscovery/types"
+	"github.com/aws/smithy-go/middleware"
+	smithyhttp "github.com/aws/smithy-go/transport/http"
+	"github.com/cenkalti/backoff/v5"
+)
+
+var namespace string
+var healthStatus string
+var retryCount int
+var noFail bool
+var executionDelayJitter time.Duration
+var executionDelayJitterUnit time.Duration
+
+func init() {
+	flag.StringVar(&namespace, "namespace", "", "The namespace of the instance to be listed")
+	flag.StringVar(&healthStatus, "health-status", "HEALTHY", "The health status of the instance to be listed")
+	flag.IntVar(&retryCount, "retry-count", 10, "The number of times to retry the request")
+	flag.BoolVar(&noFail, "no-fail", false, "Do not fail if no instances are found")
+	flag.DurationVar(&executionDelayJitter, "execution-delay-jitter", 0, "The amount of jitter that delays the command execution.")
+	flag.DurationVar(&executionDelayJitterUnit, "execution-delay-jitter-unit", time.Second, "The unit of the jitter that delays the command execution")
+}
+
+type entry struct {
+	IPv4Addr string
+	IPv6Addr string
+	Port     int
+}
+
+type serviceDiscovery struct {
+	svc       *servicediscovery.Client
+	namespace string
+	hsf       servicediscovery_types.HealthStatusFilter
+	maxTries  int
+}
+
+func (sd *serviceDiscovery) do(ctx context.Context, service string) ([]entry, error) {
+	return backoff.Retry(
+		ctx,
+		func() ([]entry, error) {
+			out, err := sd.svc.DiscoverInstances(ctx, &servicediscovery.DiscoverInstancesInput{
+				NamespaceName: aws.String(sd.namespace),
+				ServiceName:   aws.String(service),
+				HealthStatus:  sd.hsf,
+			})
+			if err != nil {
+				return nil, backoff.Permanent(fmt.Errorf("failed to discover instances: %w", err))
+			}
+			entries := make([]entry, 0, len(out.Instances))
+			for _, instance := range out.Instances {
+				ipv4Addr := ""
+				ipv6Addr := ""
+				port := 0
+				if v, ok := instance.Attributes["AWS_INSTANCE_IPV4"]; ok {
+					ipv4Addr = v
+				}
+				if v, ok := instance.Attributes["AWS_INSTANCE_IPV6"]; ok {
+					ipv6Addr = v
+				}
+				if v, ok := instance.Attributes["AWS_INSTANCE_PORT"]; ok {
+					port, err = strconv.Atoi(v)
+					if err != nil {
+						return nil, fmt.Errorf("failed to convert port to int: %w", err)
+					}
+				}
+				entries = append(entries, entry{IPv4Addr: ipv4Addr, IPv6Addr: ipv6Addr, Port: port})
+			}
+			if len(entries) == 0 {
+				return nil, errors.New("no instances found")
+			}
+			return entries, nil
+		},
+		backoff.WithBackOff(
+			&backoff.ExponentialBackOff{
+				InitialInterval:     2 * time.Second,
+				RandomizationFactor: backoff.DefaultRandomizationFactor,
+				Multiplier:          backoff.DefaultMultiplier,
+				MaxInterval:         60 * time.Second,
+			},
+		),
+		backoff.WithMaxTries(uint(sd.maxTries)),
+	)
+}
+
+// disableEndpointPrefix applies the flag that will prevent any
+// operation-specific host prefix from being applied
+type disableEndpointPrefix struct{}
+
+func (disableEndpointPrefix) ID() string { return "disableEndpointPrefix" }
+
+func (disableEndpointPrefix) HandleInitialize(
+	ctx context.Context, in middleware.InitializeInput, next middleware.InitializeHandler,
+) (middleware.InitializeOutput, middleware.Metadata, error) {
+	ctx = smithyhttp.SetHostnameImmutable(ctx, true)
+	return next.HandleInitialize(ctx, in)
+}
+
+func addDisableEndpointPrefix(stack *middleware.Stack) error {
+	return stack.Initialize.Add(disableEndpointPrefix{}, middleware.After)
+}
+
+func getValueByKey(rv reflect.Value, field string) (any, error) {
+	switch rv.Kind() {
+	case reflect.Struct:
+		f := rv.FieldByName(field)
+		if !f.IsValid() {
+			return "", fmt.Errorf("field %s not found in struct", field)
+		}
+		return f.Interface(), nil
+	case reflect.Map:
+		if rv.Type().Key().Kind() != reflect.String && rv.Type().Key().Kind() != reflect.Interface {
+			return "", fmt.Errorf("expected string or interface key, got %s", rv.Type().Key().Kind())
+		}
+		vv := rv.MapIndex(reflect.ValueOf(field))
+		if !vv.IsValid() {
+			return "", fmt.Errorf("key %s not found in map", field)
+		}
+		return vv.Interface(), nil
+	default:
+		return "", fmt.Errorf("expected struct or string keyied map, got %s", rv.Kind())
+	}
+}
+
+func convertStringSliceToAnySlice(entries []string) []any {
+	retval := make([]any, len(entries))
+	for i, entry := range entries {
+		retval[i] = entry
+	}
+	return retval
+}
+
+func getActualValueOf(rv reflect.Value) reflect.Value {
+	for rv.Kind() == reflect.Ptr || rv.Kind() == reflect.Interface {
+		rv = rv.Elem()
+	}
+	return rv
+}
+
+func doIt(ctx context.Context, logger *slog.Logger) error {
+	if namespace == "" {
+		return fmt.Errorf("namespace is required")
+	}
+	if retryCount < 0 {
+		return fmt.Errorf("retry count must be greater than or equal to 0")
+	}
+	hsf := servicediscovery_types.HealthStatusFilter(healthStatus)
+	i := slices.Index[[]servicediscovery_types.HealthStatusFilter](
+		servicediscovery_types.HealthStatusFilterAll.Values(),
+		hsf,
+	)
+	if i < 0 {
+		return fmt.Errorf("invalid health status: %s", healthStatus)
+	}
+	cmdLine := flag.Args()
+	if len(cmdLine) < 1 {
+		return fmt.Errorf("command is required")
+	}
+	var err error
+	cmdLine[0], err = exec.LookPath(cmdLine[0])
+	if err != nil {
+		return fmt.Errorf("command not found: %s", cmdLine[0])
+	}
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return err
+	}
+	options := make([]func(*servicediscovery.Options), 0, 1)
+	if cfg.BaseEndpoint != nil {
+		options = append(options, servicediscovery.WithAPIOptions(addDisableEndpointPrefix))
+	}
+	svc := servicediscovery.NewFromConfig(
+		cfg,
+		options...,
+	)
+	{
+		loggerOpts := []any{
+			slog.String("aws_region", cfg.Region),
+			slog.String("namespace", namespace),
+			slog.String("health_status", healthStatus),
+			slog.Int("retries", retryCount),
+			slog.Bool("no_fail", noFail),
+		}
+		if cfg.BaseEndpoint != nil {
+			loggerOpts = append(loggerOpts, slog.String("aws_endpoint", *cfg.BaseEndpoint))
+		}
+		logger.Info(
+			"service discovery will be performed",
+			loggerOpts...,
+		)
+	}
+	sd := &serviceDiscovery{
+		svc:       svc,
+		namespace: namespace,
+		hsf:       hsf,
+		maxTries:  retryCount + 1,
+	}
+
+	ifAddrCache := make(map[string]string)
+	funcMap := template.FuncMap{
+		"instances": func(service string) ([]entry, error) {
+			entries, err := sd.do(ctx, service)
+			if err != nil {
+				if !noFail {
+					return nil, err
+				}
+			}
+			return entries, nil
+		},
+		"exclude": func(addr string, entries []entry) ([]entry, error) {
+			retval := make([]entry, 0, len(entries))
+			for _, entry := range entries {
+				if entry.IPv4Addr == addr || entry.IPv6Addr == addr {
+					continue
+				}
+				retval = append(retval, entry)
+			}
+			return retval, nil
+		},
+		"extract": func(field string, entries any) ([]any, error) {
+			fields := strings.Split(field, ",")
+			for i, field := range fields {
+				fields[i] = strings.TrimSpace(field)
+			}
+			rv := getActualValueOf(reflect.ValueOf(entries))
+			if rv.Kind() != reflect.Slice && rv.Kind() != reflect.Array {
+				return nil, fmt.Errorf("expected array or slice, got %s", rv.Kind())
+			}
+			retval := make([]any, rv.Len())
+			if len(fields) == 1 {
+				for i := 0; i < rv.Len(); i++ {
+					v, err := getValueByKey(getActualValueOf(rv.Index(i)), fields[0])
+					if err != nil {
+						return nil, fmt.Errorf("failed to get value by key %s: %w", field, err)
+					}
+					retval[i] = v
+				}
+			} else {
+				for i := 0; i < rv.Len(); i++ {
+					vv := make([]any, len(fields))
+					for j, field := range fields {
+						v, err := getValueByKey(getActualValueOf(rv.Index(i)), field)
+						if err != nil {
+							return nil, fmt.Errorf("failed to get value by key %s: %w", field, err)
+						}
+						vv[j] = v
+					}
+					retval[i] = vv
+				}
+			}
+			return retval, nil
+		},
+		"mapprintf": func(format string, entries any) ([]string, error) {
+			switch entries := entries.(type) {
+			case []string:
+				retval := make([]string, len(entries))
+				for i, entry := range entries {
+					retval[i] = fmt.Sprintf(format, entry)
+				}
+				return retval, nil
+			case [][]any:
+				retval := make([]string, len(entries))
+				for i, entry := range entries {
+					retval[i] = fmt.Sprintf(format, entry...)
+				}
+				return retval, nil
+			case [][]string:
+				retval := make([]string, len(entries))
+				for i, entry := range entries {
+					retval[i] = fmt.Sprintf(format, convertStringSliceToAnySlice(entry)...)
+				}
+				return retval, nil
+			default:
+				rentries := getActualValueOf(reflect.ValueOf(entries))
+				if rentries.Kind() != reflect.Slice && rentries.Kind() != reflect.Array {
+					return nil, fmt.Errorf("expected []any, or [#]any, got %s", rentries.Kind())
+				}
+				retval := make([]string, rentries.Len())
+				for i := range retval {
+					rentry := getActualValueOf(rentries.Index(i))
+					if rentry.Kind() != reflect.Slice && rentry.Kind() != reflect.Array {
+						retval[i] = fmt.Sprintf(format, rentry.Interface())
+					} else {
+						args := make([]any, rentry.Len())
+						for j := 0; j < rentry.Len(); j++ {
+							args[j] = rentry.Index(j).Interface()
+						}
+						retval[i] = fmt.Sprintf(format, args...)
+					}
+				}
+				return retval, nil
+			}
+		},
+		"join": func(sep string, entries any) (string, error) {
+			switch entries := entries.(type) {
+			case []string:
+				return strings.Join(entries, sep), nil
+			case []any:
+				var result strings.Builder
+				for i, entry := range entries {
+					if i > 0 {
+						result.WriteString(sep)
+					}
+					result.WriteString(getActualValueOf(reflect.ValueOf(entry)).String())
+				}
+				return result.String(), nil
+			default:
+				return "", fmt.Errorf("expected []string or []any, got %s", reflect.TypeOf(entries))
+			}
+		},
+		"ifaddr": func(cidr string) (string, error) {
+			if addrStr, ok := ifAddrCache[cidr]; ok {
+				return addrStr, nil
+			}
+			pfx, err := netip.ParsePrefix(cidr)
+			if err != nil {
+				return "", fmt.Errorf("failed to parse CIDR: %w", err)
+			}
+			ifs, err := net.Interfaces()
+			if err != nil {
+				return "", fmt.Errorf("failed to get interfaces: %w", err)
+			}
+			for _, if_ := range ifs {
+				addrs, err := if_.Addrs()
+				if err != nil {
+					return "", fmt.Errorf("failed to get interface addresses: %w", err)
+				}
+				if if_.Flags&net.FlagUp == 0 {
+					continue
+				}
+				if if_.Flags&net.FlagPointToPoint != 0 {
+					continue
+				}
+				if if_.Flags&net.FlagLoopback != 0 {
+					continue
+				}
+				for _, addr := range addrs {
+					ip, err := netip.ParsePrefix(addr.String())
+					if err != nil {
+						return "", fmt.Errorf("failed to parse address: %w", err)
+					}
+					if pfx.Contains(ip.Addr()) {
+						addrStr := ip.Addr().String()
+						ifAddrCache[cidr] = addrStr
+						return addrStr, nil
+					}
+				}
+			}
+			return "", fmt.Errorf("no applicable interfaces found")
+		},
+	}
+
+	cmdLineT := make([]*template.Template, len(cmdLine)-1)
+	for i, arg := range cmdLine[1:] {
+		t, err := template.New(strconv.Itoa(i)).Funcs(funcMap).Parse(arg)
+		if err != nil {
+			return fmt.Errorf("failed to parse command line: %w", err)
+		}
+		cmdLineT[i] = t
+	}
+
+	delay := executionDelayJitterUnit * time.Duration(
+		rand.Int64N(
+			int64(executionDelayJitter)/int64(executionDelayJitterUnit)+1,
+		),
+	)
+	logger.Info("delaying execution", slog.Duration("delay", delay))
+	time.Sleep(delay)
+
+	renderedCmdLine := make([]string, len(cmdLine))
+	renderedCmdLine[0] = cmdLine[0]
+	for i, t := range cmdLineT {
+		var buf bytes.Buffer
+		if err := t.Execute(&buf, nil); err != nil {
+			return fmt.Errorf("failed to execute template: %w", err)
+		}
+		renderedCmdLine[i+1] = buf.String()
+	}
+
+	logger.Info("running", slog.Any("argv", renderedCmdLine))
+	cmd := exec.CommandContext(ctx, renderedCmdLine[0], renderedCmdLine[1:]...)
+	cmd.Env = os.Environ()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to run command: %w", err)
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	if err := doIt(ctx, logger); err != nil {
+		logger.Error("failed", slog.Any("err", err))
+		os.Exit(1)
+	}
+}
